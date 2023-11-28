30a3a4179f0f48c02fafa1dfd45ff4f7f35fecab
Angela Prestano <angiprestano99@gmail.com>
2023-11-28 17:58:05 +0100

    push

---
+++ src/Esercizio1/Main1.java
@@ -0,0 +1,25 @@
+package Esercizio1;
+
+public class Main1 {
+    public static boolean annoBisestile(int year) {
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
+}
+
+
+
+
+
+
